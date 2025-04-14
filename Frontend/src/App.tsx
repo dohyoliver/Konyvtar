a4c5f71be72a878df47ae070f177200999651b54
John Doe <johndoe@email.com>
2025-04-14 12:25:37 +0200

    görgetés nav

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,27 +8,40 @@ import Newbook from './Components/Newbook';
 
 
 function App() {
-  const [active, setActive] = useState('books')
+  const booksRef = useRef<HTMLDivElement>(null);
+  const felvetelRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = (ref: React.RefObject<HTMLDivElement | null>) => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <>
      <div className='container'>
-     <header className='py-3'>
-      <h1>Petrik könyvklub</h1>
-      <nav>
-        <a href='#' onClick={(e) =>{e.preventDefault(); setActive('books')}} style={{marginRight:'10px'}}>Books</a>
-        <a href='#' onClick={(e) =>{e.preventDefault(); setActive('felvetel');}}>Új tag felvétele</a>
-      </nav>
-     </header>
-     </div>
-      <div className='conteiner'>
-     <main>
- 
-      {active === 'books' && <List/>}
-      {active === 'felvetel' && <Newbook onBookAdded={() => {}}/>}
- 
-     </main>
-     </div>
+        <header className='py-3'>
+          <h1>Petrik könyvklub</h1>
+          <nav>
+            <a href='#' onClick={(e) => { e.preventDefault(); scrollToSection(booksRef); }} style={{ marginRight: '10px' }}>Books</a>
+            <a href='#' onClick={(e) => { e.preventDefault(); scrollToSection(felvetelRef); }}>Új tag felvétele</a>
+          </nav>
+        </header>
+      </div>
+
+      <div className='container'>
+        <main>
+          <div ref={booksRef}>
+            <h2>Könyvek</h2>
+            <List />
+          </div>
+          <div ref={felvetelRef} style={{ marginTop: '100px' }}>
+            <h2>Új tag felvétele</h2>
+            <Newbook onBookAdded={() => { }} />
+          </div>
+        </main>
+      </div>
+      <div className='container'>
+        <footer style={{textAlign:'center'}}>Dohy Olivér 13.E</footer>
+      </div>
     </>
   )
 }
